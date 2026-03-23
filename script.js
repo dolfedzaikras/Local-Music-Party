11174db60c78d0490598eb69bcf9257c4c2d4603
icntdthnmr <BlackAr04@gmail.com>
2026-03-24 00:16:45 +0300

    исправлена адаптивка + небольшие правки

--- script.js
+++ script.js
@@ -150,3 +150,19 @@ if (contactForm) {
   });
 }
 
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
